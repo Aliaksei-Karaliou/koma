c9307c9ecf34a58e519d70bd9f61198329727849
Aliaksei Karaliou <alexeykoroliov@gmail.com>
2019-10-10 01:07:35 +0300

    Initial commit

---
+++ matrix/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    `java-library`
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
---
+++ matrix/src/main/kotlin/com/github/aliakseikaraliou/koma/matrix/Library.kt
@@ -0,0 +1,11 @@
+package com.github.aliakseikaraliou.koma.matrix
+
+class Library {
+    fun someLibraryMethod(): Boolean {
+        return true
+    }
+
+    fun main(){
+
+    }
+}
